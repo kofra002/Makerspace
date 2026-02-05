9f2f1e2c6b1e835148004f3df0a913802c7b899a
kofra002 <kofra002@osloskolen.no>
2026-02-05 14:37:30 +0100

    Er Makerspace åpent? JavaScript

--- openingHours.js
+++ openingHours.js
@@ -1,6 +1,29 @@
+// Importerer arrays fra ./schedule.json 
+import schedule from './schedule.json' with { type: 'json' }
+
 let date = new Date()
+let i = 0
+let found = false
+let status, since, until = ""
 
-let phase = Math.floor((date.getHours() * 60 + date.getMinutes()) / 15)
+let isItOpen = document.getElementById("isItOpen")
 
-console.log(date.getDay())
-console.log(phase)
+let minuteTime = date.getHours() * 60 + date.getMinutes()
+
+while (i < schedule.length && found === false) {
+    // Her finner programmet dagen i array, deretter går den gradvis oppover fra bunnen og gir tidspunkt som 09:15
+    let targetTime = schedule[date.getDay()][i].match(/^[^-]*/)
+    // For å sammenlikne nåtid med tidspunkt for Makerspace er det overført til minutter etter midnatt
+    let targetMinute = Number(targetTime[0].match(/^[^:]*/)[0]) * 60 + Number(targetTime[0].match(/[^:]*$/)[0])
+    
+    if (targetMinute >= minuteTime) {
+        status = schedule[date.getDay()][i - 1].match(/[^-]*$/)[0]
+        since = schedule[date.getDay()][i - 1].match(/^[^-]*/)
+        until = schedule[date.getDay()][i].match(/^[^-]*/)
+        isItOpen.innerHTML = `${status}: ${since} &rarr; ${until}`
+
+        found = true
+    }
+
+    i++
+}
