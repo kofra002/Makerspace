08cf594b0bb834f9c673a4b4727c1f5e1bdada13
kofra002 <kofra002@osloskolen.no>
2026-02-07 14:28:34 +0100

    Update warning.js

--- assets/JavaScript/warning.js
+++ assets/JavaScript/warning.js
@@ -1,6 +1,7 @@
 warningElement = document.querySelector(".alert")
 contentElement = document.querySelector(".alert-text")
 
+// Hvis variablen inneholder noe innhold (altså ikke "") vil det bli presentert i en varslingsboks
 let content = ""
 
 if (content !== "") {
