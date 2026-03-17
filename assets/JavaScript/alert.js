dad0d8abc9c375cae8b6f6ec7a47ed46868d7bf3
kofra002 <kofra002@osloskolen.no>
2026-03-17 11:41:44 +0100

    Folder restructure re-commit

--- assets/JavaScript/alert.js
+++
@@ -1,9 +0,0 @@
-import alert from "/assets/JSON/alert.json" with { type: 'json' }
-
-const alertElement = document.querySelector(".alert")
-const alertContentElement = document.querySelector(".alert-text")
-
-if (alert.active) {
-    alertElement.style.display = "flex"
-    alertContentElement.innerHTML = alert.content
-}
